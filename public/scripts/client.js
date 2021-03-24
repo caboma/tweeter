70b3ddb6537d9b4e12c79e20cd66d3953dbef9ce
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-24 02:39:14 +0000

    Update tweet components to be dynamically created

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function(){
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    
+    for (let tweet in tweets) {
+      $tweet = createTweetElement(tweets[tweet]);
+      $(".inner-container").append($tweet);
+    }
+  }
+  
+  const createTweetElement = function (tweet) {
+    let $tweet = `
+    <article class="tweet">
+      <header>
+        <img class="avatar" src="${tweet.user.avatars}">
+        <h3>${tweet.user.name}</h3>
+        <p>${tweet.user.handle}</p>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+        <p>${tweet.created_at}</p>
+        <p>
+          <i class="btn fas fa-flag"></i>
+          <i class="btn fas fa-retweet"></i>
+          <i class="btn fas fa-heart"></i>
+        </p>
+      </footer>
+    </article>
+  `;
+  return $tweet;
+  }
+  renderTweets(data);
+})
+
+
