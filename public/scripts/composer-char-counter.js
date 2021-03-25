a77363e658ee182b792dacc6d2d0e46957955fa2
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-25 20:27:42 +0000

    Update error message

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-  // --- our code goes here ---
-  
+  // Counting the textarea length in new-tweet section
   $('.new-tweet #tweet-text').keyup(function () {
     let count = 140 - $(this).val().length;
+    //Update the counter diplay in output element with counter class
     $('.footer .counter').html(count)
     if(count < 0) {
       $('.footer .counter').css('color', 'red')
     }
+    if(count >= 0) {
+      $('.alertMessage').fadeOut(1000)
+    }
+    
   })
 });
